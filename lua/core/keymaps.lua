974b3d6bda9abdd24553059927ecbe359fc621e0
Maciej Borkowski <mmborkowski11@gmail.com>
2024-05-12 12:55:21 +0200

    feat: scribe range

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -13,9 +13,9 @@ keymap.set("n", "<leader>h", vim.cmd.nohlsearch, opts) -- clear highlights
 keymap.set("n", "<leader>L", "<cmd>Lazy<CR>", { noremap = true, silent = true }) -- open Lazy
 keymap.set("n", "<leader>M", "<cmd>Mason<CR>", { silent = true }) -- open Mason
 keymap.set("n", "<leader>G", "<cmd>Git<CR>", { noremap = true, silent = true }) -- open Fugitive
-keymap.set("n", "<leader>NO", ":lua require('scribe').open()<CR>", { silent = true }) -- open scribe
-keymap.set("n", "<leader>NC", ":lua require('scribe').close()<CR>", { silent = true }) -- close scribe
-keymap.set("n", "<leader>n", ":lua require('scribe').toggle()<CR>", { silent = true }) -- toggle scribe
+keymap.set("n", "<leader>E", ":NvimTreeToggle<CR>") -- toggle nvimtree
+keymap.set("n", "<leader>U", ":UndotreeToggle<CR>") -- toggle Undotree
+keymap.set("n", "<leader>n", ":ScribeToggle<CR>", { silent = true }) -- toggle scribe
 
 -- window keymaps
 keymap.set("n", "<leader>sv", "<C-w>v") -- split vertically
@@ -42,13 +42,12 @@ keymap.set("n", "<leader>tp", ":tabp<CR>") -- go to previous tab
 
 -- plugin keymaps
 keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- maximize tab
-keymap.set("n", "<leader>E", ":NvimTreeToggle<CR>") -- toggle nvimtree
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
 keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
-keymap.set("n", "<leader>U", ":UndotreeToggle<CR>") -- toggle Undotree
+keymap.set("v", "<leader>a", ":ScribeRange<CR>") -- append range to notes
 
 -- language specific keymaps
 keymap.set("n", "<leader>ee", "oif err != nil {<CR>}<Esc>Oreturn err<Esc>")
@@ -56,3 +55,4 @@ keymap.set("n", "<leader>ee", "oif err != nil {<CR>}<Esc>Oreturn err<Esc>")
 -- plugin dev keymaps
 keymap.set("n", ",r", ":lua R('scribe')<CR>", { silent = true }) -- reload scribe
 keymap.set("n", ",t", ":PlenaryBustedFile %<CR>") -- test single file with with plenary
+keymap.set("n", ",b", ":buffers<CR>", { silent = true }) -- list buffers
