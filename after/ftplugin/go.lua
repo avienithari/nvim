c219c3e25716650a1b650675b599a9465957b74a
Maciej Borkowski <mmborkowski11@gmail.com>
2024-11-27 14:24:32 +0100

    feat: i aint using it

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -1,5 +1,1 @@
-vim.keymap.set("n", "<leader>td", function()
-  require("dap-go").debug_test()
-end, { buffer = 0 })
-
 vim.keymap.set("n", "<leader>ee", "oif err != nil {<CR>}<Esc>Oreturn err<Esc>", { buffer = 0 })
--- after/ftplugin/python.lua
+++
@@ -1,3 +0,0 @@
-vim.keymap.set("n", "<leader>td", function()
-  require("dap-python").test_method()
-end, { buffer = 0 })
